fd113dac1f4d0f57528418add103ddc6d4535a68
spineda2019 <spineda.wpi.alum@gmail.com>
2025-11-23 01:47:03 -0500

    center label

--- simba/src/main.rs
+++ simba/src/main.rs
@@ -5,8 +5,8 @@ mod views;
 
 fn main() -> Result<(), FltkError> {
     let mut window = views::main_window::MainWindow::new(
-        common::Width(340),
-        common::Height(340),
+        common::Width(600),
+        common::Height(600),
         "Hello FLTK-rs",
     );
     window.show();
--- simba/src/views/main_window.rs
+++ simba/src/views/main_window.rs
@@ -8,8 +8,13 @@ pub struct MainWindow {
 impl MainWindow {
     pub fn new(width: crate::common::Width, height: crate::common::Height, title: &str) -> Self {
         let window = fltk::window::Window::new(0, 0, width.0, height.0, title);
-        let mut frame = fltk::frame::Frame::new(20, 40, width.0 / 2, height.0 / 2, title);
+
+        let frame_width = crate::common::Width(width.0 / 2);
+        let frame_start = (width.0 - frame_width.0) / 2;
+        let mut frame =
+            fltk::frame::Frame::new(frame_start, 40, frame_width.0, height.0 / 4, "Grand Total:");
         frame.set_frame(fltk::enums::FrameType::UpBox);
+        frame.set_label_font(fltk::enums::Font::TimesBold);
         frame.set_label_size(36);
         window.end();
 
