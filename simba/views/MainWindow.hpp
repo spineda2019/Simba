0d10667d1c6c596591dd768b94f7fb1717174542
spineda2019 <spineda.wpi.alum@gmail.com>
2025-11-25 23:23:44 -0500

    start lib for comptime window calculation

--- simba/views/MainWindow.hpp
+++ simba/views/MainWindow.hpp
@@ -7,6 +7,8 @@
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Window.H>
 
+#include "./common.hpp"
+
 namespace simba {
 
 class MainWindow final {
@@ -33,20 +35,24 @@ class MainWindow final {
     template <int Width, int Height>
     explicit inline MainWindow(MainWindowSize<Width, Height>) noexcept
         : window_{0, 0, Width, Height, "Simba"},
-          grand_total_box{FrameXPosition<Width>::value,
-                          FrameYPosition<Height>::value,
-                          FrameWidth<Width>::value, FrameHeight<Height>::value,
-                          "Balance: TODO"},
+          grand_total_box{FrameInfo<Width, Height>::x,
+                          FrameInfo<Width, Height>::y,
+                          FrameInfo<Width, Height>::width,
+                          FrameInfo<Width, Height>::height, "Balance: TODO"},
           load_alternate_button_{
-              ButtonXPosition<Width>::value, ButtonYPosition<Height>::value,
-              ButtonWidth<Width>::value, ButtonHeight<Height>::value,
-              "Load Other Config"} {
+              ButtonInfo<Width, Height>::x, ButtonInfo<Width, Height>::y,
+              ButtonInfo<Width, Height>::width,
+              ButtonInfo<Width, Height>::height, "Load Other Config"},
+          breakdown_button_{
+              ButtonInfo<Width, Height>::x, ButtonInfo<Width, Height>::y,
+              ButtonInfo<Width, Height>::width,
+              ButtonInfo<Width, Height>::height, "Load Other Config"} {
         //
         grand_total_box.box(FL_UP_BOX);
         grand_total_box.labelfont(FL_BOLD + FL_ITALIC);
         grand_total_box.labelsize(36);
 
-        const auto button_callback = [](Fl_Widget*, void* self) {
+        constexpr auto button_callback = [](Fl_Widget*, void* self) {
             static_cast<decltype(this)>(self)->LoadConfigClicked();
         };
         load_alternate_button_.type(FL_NORMAL_BUTTON);
@@ -55,51 +61,26 @@ class MainWindow final {
         window_.end();
     }
 
-    template <int ParentWidth>
-    struct FrameWidth {
-        inline static constexpr int value{ParentWidth / 2};
+    template <int ParentWidth, int ParentHeight>
+    struct FrameInfo {
+        inline static constexpr int width{ParentHeight / 2};
+        inline static constexpr int height{ParentHeight / 5};
+        inline static constexpr int x{(ParentWidth - width) / 2};
+        inline static constexpr int y{40};  // TOD(SEP): calculate
     };
 
-    template <int ParentHeight>
-    struct FrameHeight {
-        inline static constexpr int value{ParentHeight / 5};
-    };
-
-    template <int ParentWidth>
-    struct FrameXPosition {
-        inline static constexpr int value{
-            (ParentWidth - FrameWidth<ParentWidth>::value) / 2};
-    };
-
-    template <int ParentWidth>
-    struct FrameYPosition {
-        // TODO(SEP): Calculate
-        inline static constexpr int value{40};
-    };
-
-    template <int ParentWidth>
-    struct ButtonWidth {
-        inline static constexpr int value{ParentWidth / 4};
-    };
-
-    template <int ParentHeight>
-    struct ButtonHeight {
-        inline static constexpr int value{ParentHeight / 10};
-    };
-
-    template <int ParentWidth>
-    struct ButtonXPosition {
-        inline static constexpr int value{
-            ((ParentWidth / 2) - ButtonWidth<ParentWidth>::value) / 2};
-    };
-
-    template <int ParentHeight>
-    struct ButtonYPosition {
-        inline static constexpr int value{
-            FrameYPosition<ParentHeight>::value +
-            FrameHeight<ParentHeight>::value +
-            ((ParentHeight - (FrameYPosition<ParentHeight>::value +
-                              FrameHeight<ParentHeight>::value)) /
+    template <int ParentWidth, int ParentHeight>
+    struct ButtonInfo {
+        inline static constexpr int width{ParentWidth / 4};
+        inline static constexpr int height{ParentHeight / 10};
+        inline static constexpr int x{
+            ((ParentWidth / 2) - ButtonInfo<ParentWidth, ParentHeight>::width) /
+            2};
+        inline static constexpr int y{
+            FrameInfo<ParentWidth, ParentHeight>::y +
+            FrameInfo<ParentWidth, ParentHeight>::height +
+            ((ParentHeight - (FrameInfo<ParentWidth, ParentHeight>::y +
+                              FrameInfo<ParentWidth, ParentHeight>::height)) /
              10)};
     };
 
@@ -107,6 +88,7 @@ class MainWindow final {
     Fl_Window window_;
     Fl_Box grand_total_box;
     Fl_Button load_alternate_button_;
+    Fl_Button breakdown_button_;
 };
 }  // namespace simba
 
---
+++ simba/views/common.hpp
@@ -0,0 +1,49 @@
+// Copyright 2025 Sebastian Pineda (spineda.wpi.alum@gmail.com)
+
+#ifndef SIMBA_VIEWS_COMMON_HPP_
+#define SIMBA_VIEWS_COMMON_HPP_
+
+#include <type_traits>
+namespace simba::layout {
+
+template <int Value>
+struct RowCount {
+    static inline constexpr int value{Value};
+};
+
+template <int Value>
+struct ColumnCount {
+    static inline constexpr int value{Value};
+};
+
+template <class Type, template <auto...> class Generic>
+struct is_instance_of : std::false_type {};
+
+template <template <auto...> class Generic, auto... NTTPs>
+struct is_instance_of<Generic<NTTPs...>, Generic> : std::true_type {};
+
+template <class Type, template <auto...> class Generic>
+static inline constexpr bool is_instance_of_v =
+    is_instance_of<Type, Generic>::value;
+
+template <class T>
+concept IsRowCountType = is_instance_of_v<T, RowCount>;
+
+template <class T>
+concept IsColumnCountType = is_instance_of_v<T, ColumnCount>;
+
+template <class RowCount_t, class ColumnCount_t, int Width, int Height>
+    requires IsRowCountType<RowCount_t> && IsColumnCountType<ColumnCount_t>
+struct GridLayout {
+    inline static constexpr int cell_width{Width / ColumnCount_t::value};
+    inline static constexpr int cell_height{Height / RowCount_t::value};
+};
+
+template <int Row, int Column, class GridLayout_t>
+struct GridPosition {
+    inline static constexpr int row{Row};
+    inline static constexpr int column{Column};
+};
+}  // namespace simba::layout
+
+#endif  // SIMBA_VIEWS_COMMON_HPP_
