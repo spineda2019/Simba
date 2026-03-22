056f3565f5efe6b4e44e0f2a50e47dd8e8653762
spineda2019 <spineda.wpi.alum@gmail.com>
2026-03-22 11:58:12 -0400

    Add GPL info box

--- simba/src/mainwindow.cpp
+++ simba/src/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "moc_include/mainwindow.hpp"
 
+#include <qaction.h>
 #include <qmainwindow.h>
 #include <qwidget.h>
 #include <ui_mainwindow.h>
@@ -9,11 +10,13 @@
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow{parent}, ui_{new Ui::MainWindow} {
     ui_->setupUi(this);
+    connect(this->ui_->actionLicense_info, &QAction::triggered, this,
+            &MainWindow::OnLicenseInfoTriggered);
 }
 
 MainWindow::~MainWindow() { delete ui_; }
 
 void MainWindow::OnLicenseInfoTriggered() {
     LicenseInfo license_info{this};
-    license_info.show();
+    license_info.exec();
 }
